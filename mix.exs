f40b725843ddbb8bf2cacdf58da102602d4d398f
Lef Ioannidis <lef@unify.id>
2016-07-10 12:16:59 -0700

    Publishing to Hex

--- mix.exs
+++ mix.exs
@@ -5,8 +5,8 @@ defmodule ExKeypair.Mixfile do
     [app: :ex_keypair,
      version: "0.0.2",
      elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
+     package: package(),
+     description: description(),     
      deps: deps]
   end
 
@@ -14,19 +14,27 @@ defmodule ExKeypair.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: []]
+    []
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+
+  defp description do
+    """
+    RSA key pair generation in Elixir, using OpenSSL ports.
+    """
+  end
+
+  defp package do
+    [# These are the default files included in the package
+     name: :ex_keypair,
+     files: ["lib", "test", "config", "mix.exs", "README*", "LICENSE*" ],
+     maintainers: ["Lef Ioannidis"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/elefthei/ex_keypair",
+              "Docs" => "https://github.com/elefthei/ex_keypair"}]
   end
 end
