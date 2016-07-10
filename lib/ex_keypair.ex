69d930627fac3a1f4a61b47732551c6f8008297e
Lef Ioannidis <lef@unify.id>
2016-07-10 11:41:07 -0700

    Key generate using OpenSSL ports on the FS. Warning: not super secure

--- lib/ex_keypair.ex
+++ lib/ex_keypair.ex
@@ -1,2 +1,18 @@
 defmodule ExKeypair do
+  def keypair do
+    @doc
+    '''
+    Generate RSA key pair using openssl ports in Elixir.
+    TODO: Skip FS and generate key pair in memory
+    '''
+    {_, 0} = System.cmd "openssl", [ "genrsa", "-out", "privateKey.pem", "2048"]
+    {_, 0} = System.cmd "openssl", [ "rsa", "-pubout", "-in" ,"privateKey.pem", "-out", "publicKey.pem" ]
+    {:ok, priv} = File.read("privateKey.pem")
+    {:ok, pub} = File.read("publicKey.pem")
+    {_, 0} = System.cmd "rm", ["-f", "privateKey.pem"]
+    {_, 0} = System.cmd "rm", ["-f", "publicKey.pem"]
+
+    # Return tuple for key pair 
+    {priv, pub}
+  end
 end
