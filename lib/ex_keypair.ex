f5475184b6986db841f3337502720abd02d627a2
Lef Ioannidis <lef@unify.id>
2016-07-10 12:19:29 -0700

    NFC

--- lib/ex_keypair.ex
+++ lib/ex_keypair.ex
@@ -1,11 +1,11 @@
 defmodule ExKeypair do
+  @doc
+  '''
+  Generate RSA key pair using openssl ports in Elixir.
+  TODO: Skip FS and generate key pair in memory
+  '''
   def keypair do
-    @doc
-    '''
-    Generate RSA key pair using openssl ports in Elixir.
-    TODO: Skip FS and generate key pair in memory
-    '''
-    {_, 0} = System.cmd "openssl", [ "genrsa", "-out", "ExPrivateKey.pem", "2048"]
+   {_, 0} = System.cmd "openssl", [ "genrsa", "-out", "ExPrivateKey.pem", "2048"]
     {_, 0} = System.cmd "openssl", [ "rsa", "-pubout", "-in" ,"ExPrivateKey.pem", "-out", "ExPublicKey.pem" ]
     {:ok, priv} = File.read("ExPrivateKey.pem")
     {:ok, pub} = File.read("ExPublicKey.pem")
